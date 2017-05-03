15b2a39ef69ccd3f1371418a00cc1eb54f42775e
Niall Burkley <niallburkley@gmail.com>
2017-05-03 23:50:17 +0200

    Adding tests for social payloads

--- app/services/document_parser.rb
+++ app/services/document_parser.rb
@@ -8,6 +8,7 @@ class DocumentParser
       set_document_attributes(record, document)
       set_key_phrases(record, document)
       set_matched_keywords(record, document)
+      set_source(record, document)
 
       if document.save
         documents << document
@@ -27,6 +28,8 @@ class DocumentParser
     document.sentiment      = data['document_sentiment']
     document.url            = data['document_url']
     document.meltwater_uuid = data['document_id']
+    document.city           = data['document_city']
+    document.region         = data['document_region']
 
     if data['metadata'].present?
       document.search_name    = data['metadata']['search_name']
@@ -37,7 +40,9 @@ class DocumentParser
 
   def self.set_key_phrases(data, document)
     data['document_key_phrases'].each do |key_phrase|
-      document.key_phrases.new(phrase: key_phrase['phrase'], relevance: key_phrase['relevance'])
+      document.key_phrases.new(
+        phrase: key_phrase['phrase'], relevance: key_phrase['relevance']
+      )
     end
   end
 
@@ -45,6 +50,16 @@ class DocumentParser
     data['document_matched_keywords'].each do |keyword|
       document.matched_keywords.new(value: keyword)
     end
+  end
+
+  def self.set_source(data, document)
+    document.build_source(
+      name:             data['source_name'],
+      country_code:     data['source_country_code'],
+      sub_region:       data['source_subregion'],
+      information_type: data['source_information_type'],
+      reach:            data['source_reach']
+    )
   end
 
 end
--- spec/services/document_parser_spec.rb
+++ spec/services/document_parser_spec.rb
@@ -1,42 +1,84 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe DocumentParser do
-  before do
-    editorial_json = File.read('./spec/support/editorial_payload.json')
-    @results = DocumentParser.documents_from_json(editorial_json)
+  describe 'parsing an editorial payload' do
+    before do
+      editorial_json = File.read('./spec/support/editorial_payload.json')
+      @results = DocumentParser.documents_from_json(editorial_json)
+    end
+
+    it 'returns a document for an editorial payload' do
+      expect(@results).to      be_a(Array)
+      expect(@results.size).to eq(1)
+    end
+
+    it 'saves the correct document data' do
+      document = @results.first
+      expect(document).to                be_a(Document)
+      expect(document.title).to          eq("Musk Throws 'Pie' at Naysayers as Tesla Posts Rare Profit")
+      expect(document.image_link).to     eq("http://i1.myinforms.com/300x200/default.jpg")
+      expect(document.author).to         eq("John Doe")
+      expect(document.title).to          eq("Musk Throws 'Pie' at Naysayers as Tesla Posts Rare Profit")
+      expect(document.publish_date).to   be_within(0.1).of(Time.parse("2016-02-01T05:10:14.371Z"))
+      expect(document.language_code).to  eq("en")
+      expect(document.sentiment).to      eq("negative")
+      expect(document.url).to            eq("http://myinforms.com/en-us/a/43897824-musk-throws-pie-at-naysayers-as-tesla-posts-rare-profit/")
+      expect(document.meltwater_uuid).to eq("MweugEjvEhiuFbQQmid40Gs1Y-I")
+      expect(document.search_name).to    eq("hello")
+      expect(document.search_id).to      eq(1234)
+      expect(document.hook_id).to        eq("0a815fa3ee87489de1b932f178541f80c1f1038c")
+    end
+
+    it 'creates document key phrases' do
+      document = @results.first
+      expect(document.key_phrases.count).to eq(1)
+      expect(document.key_phrases.map(&:phrase)).to match_array('fourth quarter')
+    end
+
+    it 'creates document matched_keywords' do
+      document = @results.first
+      expect(document.matched_keywords.count).to eq(2)
+      expect(document.matched_keywords.map(&:value)).to match_array(["Musk", "Tesla"])
+    end
   end
 
-  it 'returns a document for an editorial payload' do
-    expect(@results).to      be_a(Array)
-    expect(@results.size).to eq(1)
-  end
+  describe 'parsing a social payload' do
+    before do
+      social_json = File.read('./spec/support/social_payload.json')
+      @results = DocumentParser.documents_from_json(social_json)
+    end
 
-  it 'saves the correct document data' do
-    document = @results.first
-    expect(document).to                be_a(Document)
-    expect(document.title).to          eq("Musk Throws 'Pie' at Naysayers as Tesla Posts Rare Profit")
-    expect(document.image_link).to     eq("http://i1.myinforms.com/300x200/default.jpg")
-    expect(document.author).to         eq("John Doe")
-    expect(document.title).to          eq("Musk Throws 'Pie' at Naysayers as Tesla Posts Rare Profit")
-    expect(document.publish_date).to   be_within(0.1).of(Time.parse("2016-02-01T05:10:14.371Z"))
-    expect(document.language_code).to  eq("en")
-    expect(document.sentiment).to      eq("negative")
-    expect(document.url).to            eq("http://myinforms.com/en-us/a/43897824-musk-throws-pie-at-naysayers-as-tesla-posts-rare-profit/")
-    expect(document.meltwater_uuid).to eq("MweugEjvEhiuFbQQmid40Gs1Y-I")
-    expect(document.search_name).to    eq("hello")
-    expect(document.search_id).to      eq(1234)
-    expect(document.hook_id).to        eq("0a815fa3ee87489de1b932f178541f80c1f1038c")
-  end
+    it 'returns a document for an editorial payload' do
+      expect(@results).to      be_a(Array)
+      expect(@results.size).to eq(1)
+    end
 
-  it 'creates document key phrases' do
-    document = @results.first
-    expect(document.key_phrases.count).to eq(1)
-    expect(document.key_phrases.map(&:phrase)).to match_array('fourth quarter')
-  end
+    it 'saves the correct document data' do
+      document = @results.first
+      expect(document).to                           be_a(Document)
+      expect(document.title).to                     eq("Some tweet")
+      expect(document.image_link).to                eq("http://example.com/image")
+      expect(document.author).to                    eq("Meltwater")
+      expect(document.publish_date).to              be_within(0.1).of(Time.parse("2016-02-01T05:10:14.371Z"))
+      expect(document.language_code).to             eq("en")
+      expect(document.sentiment).to                 eq("positive")
+      expect(document.url).to                       eq("https://twitter.com/Meltwater/status/841701967160311810")
+      expect(document.meltwater_uuid).to            eq("MweugEjvEhiuFbQQmid40Gs1Y-I")
+      expect(document.search_name).to               eq("social search")
+      expect(document.search_id).to                 eq(1234)
+      expect(document.hook_id).to                   eq("0a815fa3ee87489de1b932f178541f80c1f1038c")
+      expect(document.city).to                      eq("San Francisco")
+      expect(document.region).to                    eq("California")
+    end
 
-  it 'creates document matched_keywords' do
-    document = @results.first
-    expect(document.matched_keywords.count).to eq(2)
-    expect(document.matched_keywords.map(&:value)).to match_array(["Musk", "Tesla"])
+    it 'creates a document source' do
+      source = @results.first.source
+      expect(source).to                  be_a(Source)
+      expect(source.name).to             eq("twitter")
+      expect(source.country_code).to     eq("us")
+      expect(source.sub_region).to       eq("MA")
+      expect(source.information_type).to eq("social")
+      expect(source.reach).to            eq(18700)
+    end
   end
 end
